2fa8aff99c2698ab2a24a82757d3c195b91915a6
Govind S <govindsys1008@gmail.com>
2026-04-24 15:30:49 +0530

    extractService done

--- extractService/main.py
+++ extractService/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
+from utils.extractor import extra
 import os, json, boto3
 from google.cloud import pubsub_v1
 credentials_path=os.getenv("cred")
@@ -22,6 +23,18 @@ def extr(request: Request):
         dcd=base64.b64decode(data).decode("utf-8")
         payload=json.loads(dcd)
         file_key, email=payload.get("file_key"), payload.get("email")
+        skills, year, domain, text_hash, text=extra(file_key)
+        ou={"email": email, "skills": skills, "domain": domain, "text": text, "text_hash": text_hash, "year": year}
+        ot=json.dumps(ou).encode("utf-8")
+        pu=publisher.publish(EXTRACT_TOPIC, ot)
+        logging.info(f"pub: {pu.result()}")
+        return {"status": "processed"}
+    except Exception as e:
+        logger.error(f"error: {str(e)}")
+        return {"status" : "failed"}
 
+        
+
+        
 
 
