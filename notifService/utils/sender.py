a13423156c3383a01500f7131210e2a1af77be79
Govind S <govindsys1008@gmail.com>
2026-04-27 22:57:23 +0530

    send email func done

--- notifService/utils/sender.py
+++ notifService/utils/sender.py
@@ -1,3 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from dotenv import load_dotenv
+load_dotenv()
+sender_email=os.getenv("MY_EMAIL")
+app_password=os.getenv("APP_PASSWORD")
+
 def send_email(bestRes, email):
-    pass
-#TODO: Complete this by tomorrow
+    subject="Your resume has matched this Job"
+    body= f"details: {bestRes}"
+    msg = MIMEMultipart()
+    msg["From"]=sender_email
+    msg["To"]=email
+    msg["Subject"]=subject
+    msg.attach(MIMEText(body, "plain"))
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_email, app_password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.quit()
+        print("email sent ji..")
+    except Exception as e:
+        print(f"error: {str(e)}")
