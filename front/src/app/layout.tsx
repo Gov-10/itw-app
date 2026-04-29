4ba9435559d192f828b84b2e86cc1728212fba7c
Govind S <govindsys1008@gmail.com>
2026-04-29 17:59:53 +0530

    .ba

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,7 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "../components/Providers";
+import Providers from "components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
